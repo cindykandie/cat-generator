260875867ef78c31f164b350fdeba4538a955944
Cindy Kandie <37507239+cindykandie@users.noreply.github.com>
2020-07-30 11:50:59 +0300

    Add files via upload

---
+++ qazi2.js
@@ -0,0 +1,28 @@
+function generateCats(){
+    var image = document.createElement('img')
+    var div = document.getElementById('cat-gen')
+    image.src = "100daysassets/cat.png"
+    div.appendChild(image)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function catGenerator(){
+//     document.querySelector("img-container").display.flex;
+// }
